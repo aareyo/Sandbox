9f20c90579d635d388c1642c86c54ba88a299f28
Jaan Aaremäe <aaremae.jaan@gmail.com>
2019-11-27 00:16:22 +0200

    service can connect to db now

--- src/main/java/com/sandbox/sandbox/repository/SandboxRepository.java
+++ src/main/java/com/sandbox/sandbox/repository/SandboxRepository.java
@@ -6,33 +6,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public class SandboxRepository {
 
-    /*
-    private final JdbcTemplate jdbcTemplate;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @Autowired
     public SandboxRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-     */
-
-    @Autowired
-    public SandboxRepository() {
-        super();
-    }
-
     public Employee getEmployee(int id) {
-        /*
         String query = "SELECT * FROM EMPLOYEE WHERE ID = ?";
         Employee employee = jdbcTemplate.queryForObject(
                 query, new Object[] { id }, new EmployeeRowMapper());
 
         return employee;
-
-         */
-        return new Employee(2, "Testi", "Testaaja", "Testaajakatu 2");
     }
 }
